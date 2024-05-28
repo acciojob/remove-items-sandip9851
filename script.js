034eb2928dc5d950327122995a2006b6bf9993ee
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-28 22:24:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const selectElement = document.querySelector('#colorSelect');
+const btn = document.querySelector("#btn");
+
+function removeitem() {
+    // Get the index of the selected option
+    let selectedIndex = selectElement.selectedIndex;
+
+    // Get the selected option
+    let selectedOption = selectElement.options[selectedIndex];
+
+    // Now you can get the value or the text of the selected option
+    selectedOption.remove();
+}
+
+btn.addEventListener("click", removeitem);
